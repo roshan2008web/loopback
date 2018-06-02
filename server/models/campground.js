3a2594a9ba9066455bd5f16dbffc82acd996ca9a
Roshan Adhikari <roshan2008web@gmail.com>
2018-06-02 20:50:51 +1200

    Change data in db.json Installed nodemon. Added validationd for campground model and Reservation realation.

--- server/models/campground.js
+++ server/models/campground.js
@@ -1,5 +1,5 @@
 'use strict';
 
 module.exports = function(Campground) {
-
+    Campground.validatesLengthOf('name', {max:25,message:{max: "Name is too long"}});
 };
--- server/models/reservation.js
+++ server/models/reservation.js
@@ -1,5 +1,10 @@
 'use strict';
 
 module.exports = function(Reservation) {
-
+    Reservation.validate('startDate', dateValidator, {message: 'endDate should be after startDate'});
+    function dateValidator(err){
+        if(this.startDate >= this.endDate){
+            err();
+        }
+    }
 };
